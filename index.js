e4f6f48347196af4fa4cd43453dcedb4609ca787
waleed <waleed.abdi@moringaschool.com>
2026-09-24 14:00:03 +0300

    Implement JavaScript utility functions
    Add tax, uppercase conversion, maximum, palindrome, and discounted price functions.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+function calculateTax(amount) {
+	return amount * 0.1;
+}
 
+function convertToUpperCase(text) {
+	return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+	return Math.max(num1, num2);
+}
 
+function isPalindrome(word) {
+	return word === word.split('').reverse().join('');
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+	return originalPrice * (1 - discountPercentage / 100);
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
